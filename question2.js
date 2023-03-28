1556235bf10f06bc3f915fb2c90049aae1505093
hassanali293 <125535508+hassanali293@users.noreply.github.com>
2023-03-28 07:57:06 +0500

    Delete question2.js

--- question2.js
+++
@@ -1,8 +0,0 @@
-const name = "Muhammad Hassan Ali";
-const age = 24;
-
-if (age >= 18) {
-  console.log("Welcome "+name+" you are an adult");
-} else {
-  console.log("Sorry "+name+" you are not yet an adult");
-}
